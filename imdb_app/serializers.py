e3e8887c9dbab657544d023ad545020dc8abee12
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-30 13:28:01 +0300

    add /api/imdb/actors

--- imdb_app/serializers.py
+++ imdb_app/serializers.py
@@ -12,8 +12,8 @@ from imdb_app.models import Movie, Actor
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = '__all__'
-        # fields = ['id', 'name', 'release_year', 'duration_in_min', 'pic_url']
+        # fields = '__all__'
+        fields = ['id', 'name', 'release_year', 'duration_in_min', 'pic_url']
         # exclude = ['pic_url']
         # depth = 1
 
@@ -22,4 +22,3 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = '__all__'
-# api/imdb/actors
--- imdb_app/views.py
+++ imdb_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from imdb_app.models import *
 from imdb_app.serializers import MovieSerializer, ActorSerializer
@@ -25,10 +26,26 @@ from imdb_app.serializers import MovieSerializer, ActorSerializer
 
 
 @api_view(['GET'])
-def get_movies(request):
+def get_movies(request: Request):
     all_movies = Movie.objects.all()
+    print("initial query:", all_movies.query)
+
+    if 'name' in request.query_params:
+        all_movies = all_movies.filter(name__iexact=request.query_params['name'])
+        print("after adding name filter", all_movies.query)
+    if 'duration_from' in request.query_params:
+        all_movies = all_movies.filter(duration_in_min__gte=request.query_params['duration_from'])
+        print("after adding duration_from filter", all_movies.query)
+    if 'duration_to' in request.query_params:
+        all_movies = all_movies.filter(duration_in_min__lte=request.query_params['duration_to'])
+        print("after adding duration_to filter", all_movies.query)
+    if 'description' in request.query_params:
+        all_movies = all_movies.filter(description__icontains=request.query_params['description'])
+        print("after adding description filter", all_movies.query)
+
     serializer = MovieSerializer(instance=all_movies, many=True)
     return Response(data=serializer.data)
+
 
 @api_view(['GET'])
 def get_actors(request):
